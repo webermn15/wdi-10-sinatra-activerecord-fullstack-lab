f9a40588c8816aa1c887f2752d9b0a313bdfdd85
Michael Weber <webermn15@gmail.com>
2018-02-05 10:03:07 -0600

    added a nav, item delete functionality

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -8,6 +8,10 @@ class ApplicationController < Sinatra::Base
 		:database => 'item'
 	)
 
+	
+	use Rack::MethodOverride  # we "use" middleware in Rack-based libraries/frameworks
+	set :method_override, true
+
 	set :views, File.expand_path('../views', File.dirname(__FILE__))
 
 	get '/' do 
